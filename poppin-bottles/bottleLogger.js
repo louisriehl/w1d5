6910622a35fc825079726d6c0c9d4cf4f6fc3680
Louis Riehl <louisriehl@gmail.com>
2018-09-28 19:42:15 +0000

    added argv field integration

--- poppin-bottles/bottleLogger.js
+++ poppin-bottles/bottleLogger.js
@@ -2,7 +2,9 @@ var collection =
 {
   bottles: 0,
   caps: 0,
-  total: 0
+  total: 0,
+  bottlesFromCaps: 0,
+  bottlesFromEmpties: 0
 };
 
 function cashForBottles(cash)
@@ -11,49 +13,54 @@ function cashForBottles(cash)
 }
 
 
-function turnInCaps()
+function recycle()
 {
-  var getBack = 0;
-  var capsLost = 0;
+  if (collection.caps >= 4)
+  {
+    var getBack = 0;
+    var capsLost = 0;
 
-  capsLost = collection.caps - (collection.caps % 4);
-  getBack = capsLost / 4;
+    capsLost = collection.caps - (collection.caps % 4);
+    getBack = capsLost / 4;
 
-  collection.caps -= capsLost;
-  collection.total += getBack;
-  collection.bottles += getBack;
-  collection.caps += getBack;
+    collection.caps -= capsLost;
+    collection.total += getBack;
+    collection.bottles += getBack;
+    collection.caps += getBack;
+    collection.bottlesFromCaps += getBack;
+  }
+
+  if (collection.bottles >= 2)
+  {
+    var getBack = 0;
+    var bottlesLost = 0;
+
+    bottlesLost = collection.bottles - (collection.bottles % 2);
+    getBack = bottlesLost / 2;
+
+    collection.bottles -= bottlesLost;
+    collection.total += getBack;
+    collection.bottles += getBack;
+    collection.caps += getBack;
+    collection.bottlesFromEmpties += getBack;
+  }
 }
 
-function turnInBottles()
-{
-  var getBack = 0;
-  var bottlesLost = 0;
+var args = process.argv.slice(2);
 
-  bottlesLost = collection.bottles - (collection.bottles % 2);
-  getBack = bottlesLost / 2;
-
-  collection.bottles -= bottlesLost;
-  collection.total += getBack;
-  collection.bottles += getBack;
-  collection.caps += getBack;
-}
-
-collection.bottles = cashForBottles(30);
+collection.bottles = cashForBottles(args[0]);
 collection.total = collection.bottles;
 collection.caps = collection.bottles;
 
-console.log(collection);
+// console.log(collection);
 
 
 while ( collection.bottles >= 2 || collection.caps >= 4)
 {
-  if (collection.caps >= 4)
-    turnInCaps();
-  if (collection.bottles >= 2)
-    turnInBottles();
-  console.log(collection);
+  recycle();
 }
+
+console.log(collection);
 
 
 
